d00311564fa4991ff64905e41a7ad067a092dfd7
John Ikhanoba <oshioi@yahoo.co.uk>
2024-01-02 18:45:12 +0100

    New Updates

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
     var task2 = true;
   
     if (trueOrFalse) {
-      var task = true;
-      var task2 = false;
+      var task;
+      var task2;
     }
   
     return [task, task2];
