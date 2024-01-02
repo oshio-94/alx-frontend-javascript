3d0b53c9f37ca4be9d9e2d1780c618aca2a36107
John Ikhanoba <oshioi@yahoo.co.uk>
2024-01-02 23:08:09 +0100

    New task

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (idx in array) {
-    let value = array[idx];
-    array[idx] = appendString + value;
+  for (const idx in array) {
+    const value = array[idx];
+    array[idx] = `${appendString}${value}`;
   }
-  return array;
+  console.log(array);
 }
