99859be2c7dab3e7bc6de7a06b21998194faa6c3
John Ikhanoba <oshioi@yahoo.co.uk>
2024-01-03 13:03:33 +0100

    New task

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,7 +1,7 @@
 export default function createReportObject(employeesList) {
-  return { allEmployees: {...employeesList} }, 
-  getNumberofDepartments
-  const keys = Object.keys(employeesList);
-  const count = keys.length;
-  };
+  return { allEmployees: {employeesList} , getNumberofDepartments() {
+    let keys = Object.keys(employeesList);
+    let count = keys.length;
+    return count;
+  } }
 };
