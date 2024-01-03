49dd38c25e96dcb5b481f9511606f067fc947999
John Ikhanoba <oshioi@yahoo.co.uk>
2024-01-03 08:39:44 +0100

    New task

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,8 +1,4 @@
 export default function createEmployeesObject(departmentName, employees) {
-  const arr = [departmentName, employees];
-  const dict = arr.map(name => { 
-    return {departmentName : employees};
-  })
-  //const dict = {departmentName: employees};
-  //return dict;
+  //const dict = {};
+  return {departmentName: [...employees]};
 }
