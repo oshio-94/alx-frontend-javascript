6ba5d79c1251203e3fab0bce59e4eb207497990c
John Ikhanoba <oshioi@yahoo.co.uk>
2024-01-04 12:30:18 +0100

    new files

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -2,10 +2,10 @@ export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
-    } 
+    }
     if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
-    } 
+    }
     if (!Array.isArray(students)) {
       throw new TypeError('Students must be an array of strings');
     }
