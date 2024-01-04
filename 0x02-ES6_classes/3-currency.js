a78e720afa1361dce30feca55f6ba2f795fbe4d4
John Ikhanoba <oshioi@yahoo.co.uk>
2024-01-04 13:12:43 +0100

    new task files

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -10,22 +10,25 @@ export default class Currency {
     this._code = code;
     this._name = name;
   }
+
   set name(name) {
     if (typeof name !== 'string') {
       throw new TypeError('Not a string');
     }
     this._name = name;
   }
+
   get name() {
     return this._name;
   }
-  
+
   set code(code) {
     if (typeof code !== 'string') {
       throw new TypeError('Not a string');
     }
     this._code = code;
   }
+
   get code() {
     return this._code;
   }
