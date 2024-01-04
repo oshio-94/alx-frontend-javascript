14ca3bca6d4fc92c30e33b38d74f35e67975bee5
John Ikhanoba <oshioi@yahoo.co.uk>
2024-01-04 12:26:45 +0100

    new files

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,8 +1,8 @@
-import Classroom from "./0-classroom";
+import Classroom from './0-classroom';
 
 export default function initiaizeRooms() {
-    const first = new Classroom(19);
-    const second = new Classroom(20);
-    const third = new Classroom(34);
-    return [first, second, third];
+  const first = new Classroom(19);
+  const second = new Classroom(20);
+  const third = new Classroom(34);
+  return [first, second, third];
 }
