16d23e2712eeb2fb128f3c98930cc0a846df5817
John Ikhanoba <oshioi@yahoo.co.uk>
2024-01-08 15:17:41 +0100

    tak 1

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,5 +1,10 @@
 import getListStudents from './0-get_list_students';
 
 export default function getListStudentIds(getListStudents) {
-  const myArr = getListStudents.map((e) => {return e.id});
+  const arr = getListStudents;
+  if (Array.isArray(arr)) {
+    const myArr = getListStudents.map((e) => {return e.id});
+    return myArr;
+  } 
+  return [];
 }
