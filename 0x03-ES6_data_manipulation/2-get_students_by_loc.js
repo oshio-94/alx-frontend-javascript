1d6571c676dac9fec53bdcfb993b7049a2168f27
John Ikhanoba <oshioi@yahoo.co.uk>
2024-01-09 13:54:57 +0100

    task 7

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,8 @@
 export default function getStudentsByLocation(getListStudents, city) {
-  const list = getListStudents.filter((e) => {if (e.location == city){
-    return e;
-  }});
+  const list = getListStudents.filter((e) => {
+    if (e.location === city) {
+      return e;
+    } return 0;
+  });
   return list;
 }
