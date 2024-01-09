4aef75f7c7a339b7cb386d0065ef734dff2e04b8
John Ikhanoba <oshioi@yahoo.co.uk>
2024-01-09 14:20:02 +0100

    task 10

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,10 +1,10 @@
 export default function groceriesList() {
-  const items = new Map();
-  items.set('Apples', 10);
-  items.set('Tomatoes', 10);
-  items.set('Pasta', 1);
-  items.set('Rice', 1);
-  items.set('Banana', 5);
-
-  return items;
+  const groceries = new Map([ // Map of groceries and quantities
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+  return groceries; // Return the map
 }
